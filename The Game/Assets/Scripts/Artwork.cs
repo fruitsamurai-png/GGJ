6519d86d5c2b16ab098ac1ce9eb37d2dfed5d5e4
Hert97 <72371549+Hert97@users.noreply.github.com>
2024-01-27 22:37:28 +0800

    dk.

--- The Game/Assets/Scripts/Artwork.cs
+++ The Game/Assets/Scripts/Artwork.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Artwork : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    bool isStolen = false;
+    int replicaLevel = 1;
+    [SerializeField]
+    string artWorkName;
+    [SerializeField]
+    GameObject artWorkUI;
+
+    public bool IsStolen
     {
-        
+        get { return isStolen; }
+    }
+    public int ReplicaLevel
+    {
+        get { return replicaLevel; }
+        set { replicaLevel = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact(GameObject interactor)
     {
-        
+        //pop ui
+        artWorkUI.SetActive(true);
     }
+    public void Steal(GameObject interactor)
+    {
+        isStolen = true;
+        artWorkUI.SetActive(false);
+    }
+    public void Replace(int level)
+    {
+        artWorkUI.SetActive(false);
+
+    }
+
+
 }
