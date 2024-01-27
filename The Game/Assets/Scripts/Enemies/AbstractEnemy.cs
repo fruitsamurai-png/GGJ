6a147ab4b11cddef3184ebf3a6ca474e30808767
Bryan <usernamee19@gmail.com>
2024-01-27 18:11:20 +0800

    fov stuff

--- The Game/Assets/Scripts/Enemies/AbstractEnemy.cs
+++ The Game/Assets/Scripts/Enemies/AbstractEnemy.cs
@@ -1,12 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public abstract class Enemy
 {
-    protected Enemy(GameObject gameObject, Material fovMaterial)
+    public StateMachine m_EnemyStateMachine;
+    public GameObject m_GameObject;
+
+    public LineRenderer m_LineRenderer;
+    public float m_Fov = 30.0f;
+    public float m_ViewDistance = 5.0f;
+
+    public float m_AlertLevel = 0.0f;
+    public readonly float m_AlertGracePeriod = 2.0f;
+    public float m_AlertGraceElapsedTime = 0.0f;
+    public float m_AlertStepAmount = 0.001f;
+    public Material m_MeshMaterial;
+
+    public GameObject m_PlayerObject;
+    public bool IsPlayerInFOV = false;
+
+    protected Enemy(GameObject gameObject, Material fovMaterial, GameObject alertLevelPrefab)
     {
         m_GameObject = gameObject;
         m_LineRenderer = m_GameObject.GetComponent<LineRenderer>();
@@ -18,8 +36,11 @@ public abstract class Enemy
 
         m_LineRenderer.useWorldSpace = true;
         m_LineRenderer.positionCount = 4;
-        m_LineRenderer.widthMultiplier = 0.4f;
+        m_LineRenderer.widthMultiplier = 0.04f;
         m_LineRenderer.material = fovMaterial;
+
+        m_MeshMaterial = m_GameObject.GetComponent<MeshRenderer>().material;
+        m_PlayerObject = GameObject.FindGameObjectWithTag("Player");
     }
 
     public abstract void Start();
@@ -31,10 +52,40 @@ public abstract class Enemy
     public abstract void Noise(float noiselevel);// - will be called by the player when nearby this enemy and making noise, refer to mechanics on what this does.
     public abstract void Jailbreak(int playerLevel, float stunTime);// - stuns the enemy for specified stun time if the playerLevel is >= enemy level.
 
-    public void DrawFOVCone()
-    {
-        Vector3 foward = m_GameObject.transform.forward;
 
+    public virtual void UpdateAlertness()
+    {
+        if (IsPlayerInFOV)
+        {
+            IncreaseAlertess(m_AlertStepAmount);
+        }
+        else
+        {
+            if (m_AlertGraceElapsedTime >= m_AlertGracePeriod)
+            {
+                DecreaseAlertess(m_AlertStepAmount);
+            }
+            m_AlertGraceElapsedTime += Time.deltaTime;
+        }
+
+        m_AlertLevel = Math.Clamp(m_AlertLevel, 0.0f, 1.0f);
+        m_MeshMaterial.color= Color.LerpUnclamped(Color.white, Color.red, m_AlertLevel);
+    }
+
+    public virtual void IncreaseAlertess(float amount)
+    {
+        m_AlertLevel += amount;
+    }
+
+    public virtual void DecreaseAlertess(float amount)
+    {
+        m_AlertLevel -= amount;
+    }
+
+    public virtual void DrawFOVCone()
+    {
+        // Doesn't really work well for angles > 180..
+        Vector3 foward = m_GameObject.transform.forward;
         float rotationAngle = m_Fov / 2.0f; 
 
         Quaternion rotationQuaternion1 = Quaternion.Euler(0f, -rotationAngle, 0f);
@@ -46,27 +97,33 @@ public abstract class Enemy
         rotatedVector1.Scale(new Vector3(m_ViewDistance, m_ViewDistance, m_ViewDistance));
         rotatedVector2.Scale(new Vector3(m_ViewDistance, m_ViewDistance, m_ViewDistance));
 
+        Vector3 p1 = m_GameObject.transform.position + rotatedVector1;
+        Vector3 p2 = m_GameObject.transform.position + rotatedVector2;
+
+
+        Ray r1 = new Ray(m_GameObject.transform.position, rotatedVector1);
+        if (Physics.Raycast(r1, out RaycastHit hitInfo1))
+        {
+            if (hitInfo1.distance < m_ViewDistance)
+            {
+                p1 = hitInfo1.point;
+                IsPlayerInFOV |= (hitInfo1.transform.gameObject == m_PlayerObject);
+            }
+        }
+        Ray r2 = new Ray(m_GameObject.transform.position, rotatedVector2);
+        if (Physics.Raycast(r2, out RaycastHit hitInfo2))
+        {
+            if (hitInfo2.distance < m_ViewDistance)
+            {
+                p2 = hitInfo2.point;
+                IsPlayerInFOV |= (hitInfo2.transform.gameObject == m_PlayerObject);
+            }
+        }
+
         m_LineRenderer.SetPosition(0, m_GameObject.transform.position);
-        m_LineRenderer.SetPosition(1, m_GameObject.transform.position + rotatedVector1);
-        m_LineRenderer.SetPosition(2, m_GameObject.transform.position + rotatedVector2);
+        m_LineRenderer.SetPosition(1, p1);
+        m_LineRenderer.SetPosition(2, p2);
         m_LineRenderer.SetPosition(3, m_GameObject.transform.position);
-
-        m_ElapsedTime += Time.deltaTime;
-
-        float r = MathF.Sin(m_ElapsedTime);
-        float g = MathF.Cos(m_ElapsedTime);
-
-        m_LineRenderer.material.color = new Color(r, g, r);
     }
-
-    public StateMachine m_EnemyStateMachine;
-    public GameObject m_GameObject;
-
-    public LineRenderer m_LineRenderer;
-    public float m_Fov = 45.0f;
-    public float m_ViewDistance = 5.0f;
-
-    // Test
-    private float m_ElapsedTime = 0f;
 }
 
---
+++ The Game/Assets/Scripts/Enemies/CameraEnemyBehavior.cs
@@ -0,0 +1,210 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using Debug = UnityEngine.Debug;
+
+public class CameraIdleState : State
+{
+    //private readonly float m_IdleDuration = 2.0f;
+    //private float m_IdleTime = 0.0f;
+
+    public CameraIdleState(StateMachine sm, GameObject go)
+    {
+        m_Sm = sm;
+        m_Go = go;
+    }
+
+    public override void OnEnter()
+    {
+
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
+
+public class CameraPatrollingState : State
+{
+    private float m_ElapsedTime = 0.0f;
+
+    private readonly float m_DurationBeforeSwitching = 0.5f;
+    private readonly float m_RotationOffset = 90.0f;
+
+    private Vector3[] m_FowardVectors;
+    private int m_AngleIndex = 0;
+
+    private Vector3 m_InitialFoward;
+
+    public CameraPatrollingState(StateMachine sm, GameObject go, Vector3 forward)
+    {
+        m_Go = go;
+        m_Sm = sm;
+        m_FowardVectors = new Vector3[3];
+        m_InitialFoward = forward;
+    }
+
+    public override void OnEnter()
+    {
+        m_AngleIndex = 0;
+
+        for(int i = -1; i < 2; i++)
+        {
+            float rotationAngle = (m_RotationOffset * i) / 2.0f;
+            Quaternion rotationQuaternion1 = Quaternion.Euler(0f, rotationAngle, 0f);
+            Vector3 rotatedVector1 = rotationQuaternion1 * m_InitialFoward;
+            m_FowardVectors[i + 1] = rotatedVector1;
+        }
+        m_Go.transform.forward = m_FowardVectors[m_AngleIndex];
+    }
+
+    public override void OnUpdate()
+    {
+        m_ElapsedTime += Time.deltaTime;
+        if (m_ElapsedTime >= m_DurationBeforeSwitching)
+        {
+            m_ElapsedTime = 0.0f;
+            ChangeAngle();
+        }
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    private void ChangeAngle()
+    {
+        ++m_AngleIndex;
+        m_AngleIndex = m_AngleIndex % 3;
+        m_Go.transform.forward = m_FowardVectors[m_AngleIndex];
+    }
+}
+
+public class CameraAlertedState : State
+{
+    public GameObject m_Alterer;
+
+    public CameraAlertedState(StateMachine sm, GameObject go, GameObject alterer)
+    {
+        m_Go = go;
+        m_Sm = sm;
+        m_Alterer = alterer;
+    }
+    public override void OnEnter()
+    {
+
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
+public class CameraDistractedState : State
+{
+    private GameObject m_Alterer;
+    private float m_ElapsedTime = 0.0f;
+    private readonly float m_DistractedDuration = 5.0f;
+    public CameraDistractedState(StateMachine sm, GameObject go, GameObject alterer)
+    {
+        m_Go = go;
+        m_Sm = sm;
+        m_Alterer = alterer;
+    }
+    public override void OnEnter()
+    {
+        m_Go.transform.LookAt(m_Alterer.transform);
+    }
+
+    public override void OnUpdate()
+    {
+        m_ElapsedTime += Time.deltaTime;
+        if (m_ElapsedTime > m_DistractedDuration)
+        {
+            CameraEnemy cameraEnemy = m_Go.GetComponent<CameraEnemyBehavior>().m_Enemy;
+            m_Sm.ChangeState(new CameraPatrollingState(m_Sm, m_Go, cameraEnemy.m_InitialFoward));
+        }
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
+public class CameraEnemy : Enemy
+{
+    public Vector3 m_InitialFoward;
+
+    public CameraEnemy(GameObject go, Material fovMaterial, GameObject m_AlertLevelUIPrefab) 
+        : base(go, fovMaterial, m_AlertLevelUIPrefab)
+    {
+        m_EnemyStateMachine = new StateMachine();
+        m_GameObject = go;
+        m_InitialFoward = m_GameObject.transform.forward;
+    }
+    public override void Start()
+    {
+        m_EnemyStateMachine.ChangeState(new CameraPatrollingState(m_EnemyStateMachine, m_GameObject, m_InitialFoward));
+    }
+
+    public override void Update()
+    {
+        m_EnemyStateMachine.Update();
+    }
+    public override void NotifyDistraction(GameObject distraction)
+    {
+        Vector3 distractionSource = distraction.transform.position;
+        float squaredViewDistance = m_ViewDistance * m_ViewDistance;
+
+        if ((distractionSource - m_GameObject.transform.position).sqrMagnitude > squaredViewDistance)
+        {
+            m_EnemyStateMachine.ChangeState(new CameraDistractedState(m_EnemyStateMachine, m_GameObject, distraction));
+        }
+    }
+    public override void Alert(GameObject alerter)
+    {
+
+    }
+    public override void Noise(float noiselevel)
+    {
+
+    }
+    public override void Jailbreak(int playerLevel, float stunTime)
+    {
+
+    }
+}
+
+public class CameraEnemyBehavior : MonoBehaviour
+{
+    public GameObject m_AlertLevelUIPrefab;
+    public Material fovMaterial;
+
+    public CameraEnemy m_Enemy;
+    void Start()
+    {
+        m_Enemy = new CameraEnemy(gameObject, fovMaterial, m_AlertLevelUIPrefab);
+        m_Enemy.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Enemy.Update();
+    }
+
+}
