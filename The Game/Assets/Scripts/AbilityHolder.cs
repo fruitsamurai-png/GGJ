8da944e6e9e8fa9b7dc92fc51c54fcdedcde7df2
xvideos69 <79125596+xivd69@users.noreply.github.com>
2024-01-28 17:24:35 +0800

    ability fx

--- The Game/Assets/Scripts/AbilityHolder.cs
+++ The Game/Assets/Scripts/AbilityHolder.cs
@@ -4,58 +4,62 @@ using UnityEngine;
 
 public class AbilityHolder : MonoBehaviour
 {
-    [SerializeField]
-    private Ability ability;
-    [SerializeField]
-    private KeyCode key;
-    private float cd;
-    private float activeDur;
- 
-    enum AbilityState { Ready, Active, Cooldown }
-    AbilityState state = AbilityState.Ready;
+	[SerializeField]
+	private Ability ability;
+	[SerializeField]
+	private KeyCode key;
+	private float cd;
+	private float activeDur;
 
-    public string GetAbilityName() { return ability.abilityName; }
-    public float GetCurrentCd() { return cd; }
-    public float GetOverallCd() { return ability.cd; }
+	public AudioClip sfx;
+	public GameObject particlePrefab;
+	enum AbilityState { Ready, Active, Cooldown }
+	AbilityState state = AbilityState.Ready;
+
+	public string GetAbilityName() { return ability.abilityName; }
+	public float GetCurrentCd() { return cd; }
+	public float GetOverallCd() { return ability.cd; }
 
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        switch (state)
-        {
-            case AbilityState.Ready:
-                if(Input.GetKeyDown(key))
-                {
-                    ability.OnTrigger();
-                    state = AbilityState.Active;
-                    activeDur = ability.activeDur;
-                }
-                break;
-            case AbilityState.Active:
-                if(activeDur > 0)
-                {
-                    activeDur -= Time.deltaTime;
-                    ability.OnActive();
-                }
-                else
-                {
-                    state = AbilityState.Cooldown;
-                    cd = ability.cd;
-                }
-                break;
-            case AbilityState.Cooldown:
-                if (cd > 0)
-                {
-                    cd -= Time.deltaTime;
-                    ability.OnCooldown();
-                }
-                else
-                {
-                    state = AbilityState.Ready;
-                }
-                break;
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		switch (state)
+		{
+			case AbilityState.Ready:
+				if (Input.GetKeyDown(key))
+				{
+					ability.OnTrigger();
+					state = AbilityState.Active;
+					activeDur = ability.activeDur;
+					Instantiate(particlePrefab, transform.position + Vector3.up, Quaternion.identity);
+					GetComponent<AudioSource>().PlayOneShot(sfx);
+				}
+				break;
+			case AbilityState.Active:
+				if (activeDur > 0)
+				{
+					activeDur -= Time.deltaTime;
+					ability.OnActive();
+				}
+				else
+				{
+					state = AbilityState.Cooldown;
+					cd = ability.cd;
+				}
+				break;
+			case AbilityState.Cooldown:
+				if (cd > 0)
+				{
+					cd -= Time.deltaTime;
+					ability.OnCooldown();
+				}
+				else
+				{
+					state = AbilityState.Ready;
+				}
+				break;
+		}
+	}
 }
